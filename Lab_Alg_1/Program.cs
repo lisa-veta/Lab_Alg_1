35c1a305ea3c828afd16a61b2971391da89988aa
NastyaKuz <anastasiya3513@gmail.com>
2023-09-09 21:50:31 +0500

    Working And Two Point

--- Lab_Alg_1/Program.cs
+++ Lab_Alg_1/Program.cs
@@ -1,12 +1,56 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace Lab_Alg_1
 {
-    class Program
+    public class Program
     {
+        delegate void WorkingMethod(int[] vector);
+        WorkingMethod Wd; 
+        int maxValue = 50000;
+        int maxN = 100000;
         public static void Main(string[] args)
         {
+            new Program().Working();
+            //Working();
+        }
 
+        public void Working()
+        {
+            Wd = new SumFunc().Start;
+            string[] results = new string[maxN];
+
+            var watсh = new Stopwatch();
+            for (int n = 1; n <= maxN; n++)
+            {
+                watсh.Reset();
+                double sumWorks = 0;
+                
+                for (int count = 0; count < 5; count++)
+                {
+                    watсh.Start();
+                    Wd(CreateVector(n));
+                    watсh.Stop();
+                    double s = (double)watсh.Elapsed.Milliseconds;
+                    sumWorks += s;
+                    //Console.Write($"   {s} : {sumWorks} ;");
+                }
+                results[n - 1] = $"{n};{(double)(sumWorks)}";
+
+                Console.WriteLine($"{n} : {(double)(sumWorks)}");
+            }
+            File.WriteAllLines("C:\\Users\\user\\Desktop\\result.csv", results);
+        }
+
+        public int[] CreateVector(int n)
+        {
+            int[] array = new int[n];
+            var random = new Random();
+            for(int i = 0; i < n; i++)
+            {
+                array[i] = random.Next(0, maxValue);
+            }
+            return array;
         }
     }
 }
---
+++ Lab_Alg_1/SumFunc.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Alg_1
+{
+    public class SumFunc
+    {
+        public void Start(int[] vector)
+        {
+            int sum = 0;
+            for(int i = 0; i < vector.Length; i++)
+            {
+                sum += vector[i];
+            }
+        }
+    }
+}
